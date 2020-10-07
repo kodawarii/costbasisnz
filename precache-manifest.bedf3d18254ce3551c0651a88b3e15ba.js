38eaae4259cb770cee528bdea47b1a3affbf11fc
risou <ayamachi@outlook.com>
2020-10-08 00:20:03 +1300

    Updates

---
+++ precache-manifest.bedf3d18254ce3551c0651a88b3e15ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c289746476070f4137269a6b0b34cf77",
+    "url": "/costbasisnz/index.html"
+  },
+  {
+    "revision": "debaac3ba800cc26359c",
+    "url": "/costbasisnz/static/css/main.b88956a1.chunk.css"
+  },
+  {
+    "revision": "2a89d6df0f43a1f0d40b",
+    "url": "/costbasisnz/static/js/2.e455d391.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/costbasisnz/static/js/2.e455d391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "debaac3ba800cc26359c",
+    "url": "/costbasisnz/static/js/main.69987d64.chunk.js"
+  },
+  {
+    "revision": "95466843d4511188cf71",
+    "url": "/costbasisnz/static/js/runtime-main.eb054fe2.js"
+  }
+]);
