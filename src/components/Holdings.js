5c8baa61fb0d6e837c5812d0f245ce0b5d704946
risou <ayamachi@outlook.com>
2020-10-31 14:48:16 +1300

    Added styles for holdings

--- src/components/Holdings.js
+++ src/components/Holdings.js
@@ -31,22 +31,22 @@ class Holdings extends Component {
 
     getRows(){
         return this.props.holdings.map( (entry, i) => {
-            let totalBookValue = (entry.shares * entry.currentPrice).toFixed(4);
-            let totalGainsDollars = (totalBookValue - entry.totalInvested).toFixed(4);
+            let totalCurrValue = (entry.shares * entry.currentPrice).toFixed(0);
+            let totalGainsDollars = (totalCurrValue - entry.totalInvested).toFixed(2);
             let totalGainsPercent = ((totalGainsDollars/entry.totalInvested)*100).toFixed(2);
 
             let notesArrow1 = entry.notes1 === '' ? '' : '⭦';
 
-            return <tr key={i}>
+            return <tr key={i} className="holdings-row">
                 <td>{entry.ticker}</td>
-                <td>//DIST</td>
-                <td>{entry.shares.toFixed(4)}</td>
-                <td>{entry.totalInvested.toFixed(4)}</td>
-                <td>{entry.currentPrice.toFixed(4)}</td>
-                <td>{totalBookValue}</td>
-                <td>{totalGainsDollars}</td>
-                <td>{totalGainsPercent}%</td>
-                <td>//GAIN%</td>
+                <td><span className="holdings-distribution-wrapper">22%</span></td>
+                <td><b>{entry.shares.toFixed(0)}</b></td>
+                <td>{entry.totalInvested.toFixed(0)}</td>
+                <td>{entry.currentPrice.toFixed(2)}</td>
+                <td>{totalCurrValue}</td>
+                <td className="holdings-gains-dollars">+ {totalGainsDollars}</td>
+                <td><span className="holdings-percentOfGains-wrapper"><b>{totalGainsPercent}%</b></span></td>
+                <td>12.54%</td>
                 <td className="OpenNotes" onClick={ () => this.openNotes(entry.notes1)}> {notesArrow1} </td>
             </tr>
         });
@@ -60,21 +60,36 @@ class Holdings extends Component {
                 <h4>Current Holdings Summary</h4>
                 <table>
                     <thead>
-                        <tr>
-                            <th>Ticker Symbol</th>
-                            <th>% Distribution</th>
-                            <th>Shares</th>
-                            <th>Total Invested</th>
-                            <th>Curr Price</th>
-                            <th>Total Book</th>
-                            <th>$ G/L</th>
-                            <th>% G/L</th>
-                            <th>% of Gains</th>
-                            <th>Notes</th>
+                        <tr className="holdings-table-headers">
+                            <th>TICKER ⯅</th>
+                            <th>% DISTRIBUTION ⯆</th>
+                            <th>POSITION ⯆</th>
+                            <th>INVESTED ⯆</th>
+                            <th>CURRENT PRICE ⯅</th>
+                            <th>MARKET VALUE⯅</th>
+                            <th>$ P/L ⯅</th>
+                            <th>% P/L ⯅</th>
+                            <th>% OF GAINS ⯅</th>
+                            <th>NOTES</th>
                         </tr>
                     </thead>
                     <tbody>
                         {this.getRows()}
+                        <tr>
+
+                        </tr>
+                        <tr className="holdings-final-row">
+                            <td>Totals</td>
+                            <td></td>
+                            <td></td>
+                            <td>20,000</td>
+                            <td></td>
+                            <td>34,000</td>
+                            <td>+ 14,000</td>
+                            <td><span className="holdings-percentOfLosses-wrapper">- 3.66%</span></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
                     </tbody>
                 </table>
                 
